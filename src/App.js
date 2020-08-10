bf4b30d6cfeb20e99a5b7f392b1525a47c8232cf
Muhammad-Tayyab1 <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-08-11 00:13:38 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import {RedQueenRace}  from "./Components/Index";
+
+function App() {
+	return (
+		<div>
+			<RedQueenRace />
+		</div>
+	);
+}
+
+export default App;
